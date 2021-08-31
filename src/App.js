693766988860464d87e080b3b1b95b776bd263de
BennyZHill <ben.h@builtbypixel.com>
2021-08-31 14:26:11 +0100

    set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("")
+
+  const Login = (details) => {
+    console.log(details);
+  };
+
+  const Logout = () => {
+    console.log("Logout");
+  };
+  return ( 
+  <div className="App">
+    {(user.email !="") ? (
+    <div className="welcome">
+    <h2>Welcome, <span>{user.name}</span></h2>
     </div>
+  ):(
+  <LoginForm />
   );
-}
+}    
+
 
 export default App;
